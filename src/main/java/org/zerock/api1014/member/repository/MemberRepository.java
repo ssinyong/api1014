49910f3a96da69cc5ad698c7aff753fd526774f7
Seola CHOE <tjfdk0493@gmail.com>
2024-10-14 14:08:21 +0900

    4교시 끝

---
+++ src/main/java/org/zerock/api1014/member/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package org.zerock.api1014.member.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.zerock.api1014.member.domain.MemberEntity;
+
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+}
---
+++ src/test/java/org/zerock/api1014/member/repository/MemberRepositoryTests.java
@@ -0,0 +1,43 @@
+package org.zerock.api1014.member.repository;
+
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+import org.zerock.api1014.member.domain.MemberEntity;
+import org.zerock.api1014.member.domain.MemberRole;
+
+@SpringBootTest
+@Log4j2
+public class MemberRepositoryTests {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Commit
+    public void testDummies(){
+
+        for (int i = 0; i <= 10 ; i++) {
+            MemberEntity member = MemberEntity.builder()
+                    .email("user" + i + "@@@.com")
+                    .pw(passwordEncoder.encode("1111"))
+                    .role(i < 8 ? MemberRole.USER: MemberRole.ADMIN)
+                    .build();
+
+            memberRepository.save(member);
+
+        }
+    }
+
+}
