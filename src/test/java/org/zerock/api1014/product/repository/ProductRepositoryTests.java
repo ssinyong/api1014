b9557e05815faf1e2df620d2fc83db0db0256eb9
Seola CHOE <tjfdk0493@gmail.com>
2024-10-14 11:00:08 +0900

    2교시 끝_ProductSearch(Impl),RepositoryTests 만들기 10:55

---
+++ src/test/java/org/zerock/api1014/product/repository/ProductRepositoryTests.java
@@ -0,0 +1,15 @@
+package org.zerock.api1014.product.repository;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@Log4j2
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class ProductRepositoryTests {
+
+    @Autowired
+    private ProductRepository productRepository;
+}
