aec8f8a1a2eaf74fdf885b823f5476d7f7425f9d
Seola CHOE <tjfdk0493@gmail.com>
2024-10-14 11:22:19 +0900

    3교시 중간

--- src/main/java/org/zerock/api1014/product/repository/search/ProductSearchImpl.java
+++ src/main/java/org/zerock/api1014/product/repository/search/ProductSearchImpl.java
@@ -1,10 +1,16 @@
 package org.zerock.api1014.product.repository.search;
 
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.JPQLQuery;
+import com.querydsl.jpa.impl.JPAQuery;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.zerock.api1014.product.domain.Product;
+import org.zerock.api1014.product.domain.QAttachFile;
+import org.zerock.api1014.product.domain.QProduct;
+import org.zerock.api1014.product.domain.QReivew;
 
 @Log4j2
 public class ProductSearchImpl extends QuerydslRepositorySupport implements ProductSearch{
@@ -14,8 +20,34 @@ public class ProductSearchImpl extends QuerydslRepositorySupport implements Prod
     }
 
 
-    public Page<Product> list (Product product, Pageable pageable) {
-        log.info("=--------------------------list----------------");
+    public Page<Product> list ( Pageable pageable) {
+        log.info("--------------------------list----------------");
+
+        QProduct product = QProduct.product;
+        QReivew reivew = QReivew.reivew;
+
+        QAttachFile attachFile = QAttachFile.attachFile;
+
+
+        JPQLQuery<Product> query = from(product);
+        query.leftJoin(reivew).on(reivew.product.eq(product));
+        query.leftJoin(product.attachFiles, attachFile);
+
+        query.where(attachFile.ord.eq(0));
+        query.groupBy(product);
+
+        //페이징처리
+        this.getQuerydsl().applyPagination(pageable, query);
+
+        JPQLQuery<Tuple> tupleQuery =
+                query.select(
+                        product,
+                        reivew.count(),
+                        attachFile.fileName
+                        );
+        tupleQuery.fetch();
+
+
 
         return null;
     }
--- src/test/java/org/zerock/api1014/product/repository/ProductRepositoryTests.java
+++ src/test/java/org/zerock/api1014/product/repository/ProductRepositoryTests.java
@@ -1,9 +1,12 @@
 package org.zerock.api1014.product.repository;
 
 import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 @DataJpaTest
 @Log4j2
@@ -12,4 +15,12 @@ public class ProductRepositoryTests {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Test
+    public void testList1(){
+
+        Pageable pageable = PageRequest.of(0,10);
+
+        productRepository.list(pageable);
+    }
 }
