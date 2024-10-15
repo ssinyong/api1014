8ed1a44038799b7a724248ab22b0bc1d4438e019
ssinyong <bsy2889@gmail.com>
2024-10-15 10:46:19 +0900

    2교시 중간

---
+++ src/main/java/org/zerock/api1014/category/controller/advice/CategoryControllerAdvice.java
@@ -0,0 +1,35 @@
+package org.zerock.api1014.category.controller.advice;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+@Log4j2
+public class CategoryControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<java.util.Map<String,Object>> handle(MethodArgumentNotValidException ex) {
+
+        BindingResult bindingResult = ex.getBindingResult();
+
+        Map<String, Object> map = new HashMap<>();
+
+        ex.getFieldErrors().forEach(fieldError -> {
+            log.error("-------------");
+            String key = fieldError.getField();
+
+            Object value = fieldError.getDefaultMessage();
+            map.put(key, value);
+        });
+
+
+        return ResponseEntity.status(400).body(map);
+    }
+}
--- src/main/java/org/zerock/api1014/security/config/CustomSecurityConfig.java
+++ src/main/java/org/zerock/api1014/security/config/CustomSecurityConfig.java
@@ -3,8 +3,10 @@ package org.zerock.api1014.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class CustomSecurityConfig {
@@ -12,6 +14,17 @@ public class CustomSecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                (auth) -> auth.requestMatchers("/api/v1/category").permitAll()
+        );
+
+        http.csrf(config -> config.disable());
+
+        return http.build();
     }
 
 }
