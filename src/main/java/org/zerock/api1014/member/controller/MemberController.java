75d8484a2dc145a1cf0e53c1ecafe09798d795ff
ssinyong <bsy2889@gmail.com>
2024-10-15 14:56:54 +0900

    5교시 끝

--- src/main/java/org/zerock/api1014/member/controller/MemberController.java
+++ src/main/java/org/zerock/api1014/member/controller/MemberController.java
@@ -1,6 +1,7 @@
 package org.zerock.api1014.member.controller;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -10,11 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.api1014.member.dto.TokenRequestDTO;
 import org.zerock.api1014.member.dto.TokenResponseDTO;
+import org.zerock.api1014.member.service.MemberService;
 
 @RestController
 @RequestMapping("/api/v1/member")
 @Log4j2
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
 
     @PostMapping("makeToken")
     public ResponseEntity<TokenResponseDTO> makeToken(@RequestBody @Validated TokenRequestDTO tokenRequestDTO) {
