a9f281d7d85d5b6d6b2ecd0c650695566485f6b1
ssinyong <bsy2889@gmail.com>
2024-10-16 14:08:39 +0900

    4교시 끝!

--- src/main/java/org/zerock/api1014/product/controller/ProductController.java
+++ src/main/java/org/zerock/api1014/product/controller/ProductController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.zerock.api1014.common.dto.PageRequestDTO;
 import org.zerock.api1014.common.dto.PageResponseDTO;
 import org.zerock.api1014.product.dto.ProductListDTO;
 import org.zerock.api1014.product.service.ProductService;
+import org.zerock.api1014.security.auth.CustomUserPrincipal;
 
 @RestController
 @RequestMapping("/api/v1/product")
@@ -26,9 +28,12 @@ public class ProductController {
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("list")
     public ResponseEntity<PageResponseDTO<ProductListDTO>> list(
-            @Validated PageRequestDTO requestDTO
+            @Validated PageRequestDTO requestDTO,
+            @AuthenticationPrincipal CustomUserPrincipal principal
     ) {
         log.info("--------------------------Product Controller list");
+        log.info("==========================");
+        log.info(principal);
         return ResponseEntity.ok(productService.list(requestDTO));
     }
 }
